7113b10819c79ca821ce8dd8bd464071310fd384
AKosmachyov <a.kosmachyov@hotmail.com>
2017-06-19 14:47:49 +0300

    Added form in the "chat-template" added "infoMessage"

--- www/js/controllers/ChatController.js
+++ www/js/controllers/ChatController.js
@@ -3,29 +3,57 @@ app.controller('ChatController', function($scope, $sanitize, socket, authorizati
 	self.messages = [];
 
     socket.on('new message', function (data) {
-        if (data.message && data.username) {
-	   		self.messages.push(data);
-        }
+        let message = self.createUserMessage(data);
+        if (message)
+            self.addMessage(message);
     });
     socket.on('user joined', function (data) {
-        if(data.username && data.numUsers) {
-        }
+        let message = self.createInfoMessage(data, true);
+        if (message)
+            self.addMessage(message);
     });
     socket.on('user left', function (data) {
-        if(data.username && data.numUsers) {
-        }
+        let message = self.createInfoMessage(data, false);
+        if (message)
+            self.addMessage(message);
     });
     
-    self.addMessage = () => {
+    self.sendMessage = () => {
         let username = authorization.get();
         let text = $sanitize($scope.text);
         if (!text || !username)
             return;
-        let message = {
+        let message = self.createUserMessage({
             message: text,
             username: username
-        };
+        });
         socket.emit('new message', text);     
         self.messages.push(message);
+        $scope.text = '';
     };
+
+    self.addMessage = (message) => {
+        if(!message)
+            return null;
+        self.messages.push(message);
+    }
+    self.createUserMessage = (data, isInfo) => {
+        if (!(data.username && data.message))
+            return null;
+        return {
+            isInfo: false,
+            message: data.message,
+            username: data.username
+        }
+    }
+    self.createInfoMessage = (data, isNew) => {
+        if(!(data.username && data.numUsers))
+            return null;
+        let message = isNew ? `${data.username} joined`: `${data.username} left`;
+        return {
+            isInfo: true,
+            message: message,
+            numUsers: data.numUsers
+        }
+    }
 });
